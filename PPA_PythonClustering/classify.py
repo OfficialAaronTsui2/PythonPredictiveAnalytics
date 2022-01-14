5d98bfabfab806c561f1e32c4e51b6f90b68e427
OfficialAaronTsui2 <85973117+OfficialAaronTsui2@users.noreply.github.com>
2022-01-13 22:35:12 -0800

    clustering

--- PPA_PythonClustering/classify.py
+++ PPA_PythonClustering/classify.py
@@ -11,9 +11,14 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 import matplotlib.pyplot as plt
 
+from sklearn import linear_model
+
 df = pd.read_csv(r"C:\Users\aaron\Desktop\CICS397A\HW3\data.csv")
 
-BirchC = sklearn.cluster.Birch(df, 1, 50, 3, True)
+reg = linear_model.LinearRegression()
+reg.fit([[0,0], [2,2],[4,4]], [0,1,2])
 
-print(BirchC)
+reg.coef_
+
+
 
